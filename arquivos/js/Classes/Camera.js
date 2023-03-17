f541dc14f412236ea190a732fa1c0b95b264d2d0
Thales Mariano <thalesmariano124@gmail.com>
2023-03-17 16:21:16 -0300

    camera updates

--- arquivos/js/Classes/Camera.js
+++ arquivos/js/Classes/Camera.js
@@ -5,17 +5,18 @@ class Camera {
 		this.y = 0
 		this.width
 		this.height
+		this.xcount = 0
 	}
 
 	leftEdge(){
-		return this.x + (this.width * 0.75)
+		return this.x + (this.width/3)
 	}
 
 	rightEdge(){
-		return this.x + (this.width * 0.75)
+		return this.x + (this.width/3)
 	}
 	topEdge(){
-		return this.y + (this.height * 0.25)
+		return this.y + (this.height/2)
 	}
 	bottomEdge(){
 		return this.y + (this.height * 0.75)
@@ -26,19 +27,18 @@ class Camera {
 		this.height = canvas.height
 
 		if(player.position.x < this.leftEdge()){
-			this.x = player.position.x - (this.width * 0.25)
+			this.x = player.position.x - (this.width/3)
 		}
 
 		if(player.position.x + player.width > this.rightEdge()){
-			this.x = player.position.x + player.width - (this.width * 0.75)
+			this.x = player.position.x + player.width - (this.width/3)
 		}
-
 		if(player.position.y < this.topEdge()){
-			this.y = player.position.y - (this.height * 0.25)
+			this.y = player.position.y - (this.height/2)
 		}
 
 		if(player.position.y + player.height > this.bottomEdge()){
-				this.y = player.position.y + player.height - (this.height * 0.75)
+			this.y = player.position.y + player.height - (this.height * 0.75)
 		}
 
 		// Limites da câmera
