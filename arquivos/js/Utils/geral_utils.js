f1007d943d99ecbcefc4eee979f830103749d08b
Thales Mariano <thalesmariano124@gmail.com>
2023-03-18 17:24:31 -0300

    detectInArea from main to geral_utils

--- arquivos/js/Utils/geral_utils.js
+++ arquivos/js/Utils/geral_utils.js
@@ -7,3 +7,41 @@ function generateEnemys(amount, health){
 		enemys.push(enemy)
 	}
 }
+
+function detectInArea(entity, target, areaTotal, topArea, bottomArea, leftArea, rightArea){
+	const {overlap, distance} = collide(target, entity)
+
+	const radar = {
+		top: false,
+		bottom: false,
+		left: false,
+		right: false,
+		distance
+	}
+
+	const top    = topArea ? topArea : areaTotal
+	const bottom = bottomArea ? bottomArea : areaTotal
+	const left   = leftArea ? leftArea : areaTotal
+	const right  = rightArea ? rightArea : areaTotal
+
+	const isInTopArea = Math.abs(distance.y) < top
+	const isInBottomArea = Math.abs(distance.y) < bottom
+
+	const isInLeftArea = Math.abs(distance.x) < left
+	const isInRightArea = Math.abs(distance.x) < right
+
+	if(overlap.x >= overlap.y){
+		if(distance.y > 0){
+			radar.top = isInTopArea
+		}else{
+			radar.bottom = isInBottomArea
+		}
+	}else{
+		if(distance.x < 0){
+			radar.left = isInLeftArea
+		}else{
+			radar.right = isInRightArea
+		}
+	}
+	return radar
+}
