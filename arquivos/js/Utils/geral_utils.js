30b71e13d19eb378bfc236bc8b7073ce912d63c6
Thales Mariano <thalesmariano124@gmail.com>
2023-03-31 13:52:23 -0300

    litte fix in spawn enmys

--- arquivos/js/Utils/geral_utils.js
+++ arquivos/js/Utils/geral_utils.js
@@ -1,7 +1,7 @@
 
 function generateEnemys(amount, health){
 	for(i = 0; i < amount; i++){
-		const posX = Math.floor(Math.random() * ((mapSize - 50) - 1450 + 1) + 1450)
+		const posX = Math.floor(Math.random() * (mapSize - 1825)) + 1825
 		const enemy = new Enemy({color: "red", health: health, position: {x: posX, y: 400}})
 		enemy.setSprites(enemy_sprites)
 		enemys.push(enemy)
