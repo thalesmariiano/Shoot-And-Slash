08f3d49bd11bd599c79896ab6efb9c9dc71b00f7
Thales Mariano <thalesmariano124@gmail.com>
2023-03-30 22:47:19 -0300

    enemy drop 'soul' added

--- arquivos/js/Classes/Enemy.js
+++ arquivos/js/Classes/Enemy.js
@@ -32,6 +32,15 @@ class Enemy extends Entity {
 			if(this.currentFrames >= this.spriteFrames){
 				if(this.sprInfo.name == `dead_${this.direction.toLowerCase()}`){
 					this.endAnimation = true
+					const soul = new Item({
+						itemType: "soul",
+						position: {
+							x: this.position.x + this.width/2,
+							y: this.position.y + this.height - 32
+						}
+					})
+					soul.setSprites(itens_sprites.enemy_soul.sprites)
+					itensArray.push(soul)
 					return
 				}
 				this.currentFrames = 0
