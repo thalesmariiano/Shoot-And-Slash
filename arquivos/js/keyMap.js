5f54c974854d2264c704e76c4c44e20f7093bc2b
Thales Mariano <thalesmariano124@gmail.com>
2023-04-11 16:12:24 -0300

    controls config refacoted

--- arquivos/js/keyMap.js
+++ arquivos/js/keyMap.js
@@ -1,10 +1,32 @@
 
+const getControl = window.localStorage.getItem("SaSControl")
+if(!getControl) window.localStorage.setItem("SaSControl", 1)
+
+const getElement = type => document.querySelector(`[data-control="${type}"]`)
+
 const controlKey = {
-	left: "KeyA",
-	right: "KeyD",
-	jump: "Space",
-	jump_2: "KeyW"
+	left: null,
+	right: null,
+	jump: null,
+	jump_2: null
 }
+
+if(getControl == 1){
+	getElement(1).classList.remove("opacity-50", "hover:opacity-75")
+	getElement(2).classList.add("opacity-50", "hover:opacity-75")
+	controlKey.left   = "KeyA"
+	controlKey.right  = "KeyD"
+	controlKey.jump   = "Space"
+	controlKey.jump_2 = "KeyW"
+}else{
+	getElement(2).classList.remove("opacity-50", "hover:opacity-75")
+	getElement(1).classList.add("opacity-50", "hover:opacity-75")
+	controlKey.left   = "ArrowLeft"
+	controlKey.right  = "ArrowRight"
+	controlKey.jump   = "ArrowUp"
+	controlKey.jump_2 = "Space"
+}
+
 
 const switchControls = (control, type) => {
 	const getControl = window.localStorage.getItem("SaSControl")
